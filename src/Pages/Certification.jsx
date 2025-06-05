e398cfe486f0ccc67c2eae499a5f8a60b2b86150
Prasanthgadde-17 <prasanthgadde27@gmail.com>
2025-06-05 12:55:44 +0530

    Created

---
+++ src/Pages/Certification.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import img1 from "../assets/Intern.png";
+import img2 from "../assets/IntroductionToNet.png";
+import img3 from "../assets/NetworkEssential.png";
+import img4 from "../assets/Routing.png";
+import img5 from "../assets/Hackathon.png";
+import img6 from "../assets/UX.png";
+
+const Certification = () => {
+  const Certificates = [
+    {
+      id: 1,
+      Title: "Codex Intern",
+      short: "Frontend",
+      desc: "Completed an intensive 4-week internship focused on frontend web development.Worked on real-time projects using HTML, CSS, JavaScript, and React, gaining strong hands-on exposure.Learned to implement responsive UI components, design principles, and code optimization practices.Collaborated in a professional setting, enhancing project management and team communication skills.",
+      img: img1,
+    },
+    {
+      id: 2,
+      Title: "Introduction to Networks",
+      short: "Networking",
+      desc: "Completed Cisco's foundational networking course, gaining hands-on skills in configuring devices. Prepared for network infrastructure roles. This marks a key step in mastering network essentials.",
+      img: img2,
+    },
+    {
+      id: 3,
+      Title: "Cisco Networking Essentials",
+      short: "Networking",
+      desc: "Achieved proficiency in core network concepts, from home setups to communication protocols. Mastered IP addressing and secure internet access configuration. This certificate showcases a solid networking foundation",
+      img: img3,
+    },
+    {
+      id: 4,
+      Title: "Switching, Routing, and Wireless Essentials ",
+      short: "Network Routing",
+      desc: "Validated expertise in advanced switching, routing, and wireless LANs, including VLANs and STP. Skilled in troubleshooting, dynamic addressing, and robust network security. This highlights a versatile network management skill set",
+      img: img4,
+    },
+    {
+      id: 5,
+      Title: "Hackathon ",
+      short: "Participation",
+      desc: "Actively participated in a 2-day national-level hackathon aimed at fostering innovation and collaboration.Contributed to a tech-driven problem-solving environment with a team-focused approach.Gained hands-on experience in rapid prototyping, brainstorming, and building scalable solutions under time constraints.",
+      img: img5,
+    },
+    {
+      id: 6,
+      Title: "User Experience (UX) Design",
+      short: "UX Design",
+      desc: "Earned Google's certificate in UX Design fundamentals, focusing on user-centered principles. Equipped to craft intuitive and engaging digital experiences. This marks a foundational step in shaping user interactions with technology",
+      img: img6,
+    },
+  ];
+
+  const [selectedCertificate, setSelectedCertificate] = useState(null);
+  const [isPopupvisible, setIsPopupvisible] = useState(false);
+
+  const openPopup = (cert) => {
+    setSelectedCertificate(cert);
+    console.log(selectedCertificate);
+
+    setIsPopupvisible(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupvisible(false);
+    setSelectedCertificate(null);
+  };
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape" && selectedCertificate) {
+        closePopup();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [selectedCertificate]);
+
+  return (
+    <>
+      <div className="border border-white/40 m-5 rounded-xl shadow text-white">
+        <h2 className="text-4xl font-semibold text-center p-8">Certificates</h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-8 ">
+          {Certificates.map((cert) => (
+            <div
+              key={cert.id}
+              className=" group relative rounded-xl overflow-hidden  transform transition-all duration-300 hover:scale-105 cursor-pointer "
+              onClick={() => openPopup(cert)}
+            >
+              <img
+                src={cert.img}
+                alt="Img"
+                className="w-full h-50 sm:h-52 md:h-70 object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+              />
+              <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/80 via-black/60 to-transparent">
+                <h3 className="text-lg sm:text-xl font-semibold text-white ">
+                  {cert.Title}
+                </h3>
+                <p className="text-xs  text-light sm:text-sm text-gray-300 mt-1 ">
+                  {cert.short}
+                </p>
+                {/* <p className="hidden sm:block text-xs text-gray-400 mt-1 truncate" title={cert.desc}>
+                {cert.desc}
+              </p> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {selectedCertificate && (
+        <div
+          className={`fixed inset-0 bg-black/80 z-50 transition-opacity duration-300 flex items-center justify-center backdrop-filter backdrop-blur-sm transform ${
+            isPopupvisible ? "opacity-100" : "opacity-0"
+          }`}
+          onClick={() => closePopup()}
+        >
+          <div
+            className="info text-white bg-black/50 border border-white/50 p-5 rounded-xl shadow-lg h-130 sm:h-150 w-180 m-4"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex justify-end">
+              <button
+                className="bg-gray-800  px-3 py-2 rounded-2xl hover:bg-gray-600 transition"
+                onClick={() => closePopup()}
+              >
+                X
+              </button>
+            </div>
+            <div className="mb-4 w-full  rounded-xl shadow-lg p-4 flex flex-col items-center text-center">
+              <div className="h-65 sm:h-90  w-full  flex items-center justify-center rounded-md overflow-hidden mb-4">
+                <img
+                  src={selectedCertificate.img}
+                  alt="Certificate"
+                  className="h-full w-full object-contain"
+                />
+              </div>
+              <div className="info text-white">
+                <h1 className="text-xl font-bold mb-2">
+                  {selectedCertificate.Title}
+                </h1>
+                <h3 className="text-sm text-gray-400">
+                  {selectedCertificate.desc}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Certification;
---
+++ src/Pages/Project.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import ChromaGrid from "../Reactbits/ChromaGrid/ChromaGrid";
+
+const Project = () => {
+  const items = [
+    {
+      image:
+        "https://github.com/Prasanthgadde-17/Portfolio-website/blob/main/src/assets/shopping.png?raw=true",
+      title: "Shopping Site",
+      subtitle: "HTML5, CSS3, JS",
+      handle: "Frontend",
+      borderColor: "#6366F1",
+      gradient: "linear-gradient(180deg, #6366F1, #000)",
+      url: "https://github.com/Prasanthgadde-17/shoppingsite",
+    },
+    {
+      image:
+        "https://github.com/Prasanthgadde-17/Portfolio-website/blob/main/src/assets/todo.jpg?raw=true",
+      title: "TO-DO Backend",
+      subtitle: "React, Node, Express.js, MongoDB",
+      handle: "Full Stack",
+      borderColor: "#14B8A6",
+      gradient: "linear-gradient(180deg, #10B981, #000)",
+      url: "https://github.com/Prasanthgadde-17/Backend-todo",
+    },
+    {
+      image:
+        "https://github.com/Prasanthgadde-17/Portfolio-website/blob/main/src/assets/Social.png?raw=true",
+      title: "Social Network",
+      subtitle: "MERN Stack, Material-UI, Tailwind",
+      handle: "Full Stack",
+      borderColor: "#ffff",
+      gradient: "linear-gradient(180deg, #ffff, #000)",
+      url: "https://github.com/Prasanthgadde-17/Social-Network",
+    },
+    {
+      image:
+        "https://github.com/Prasanthgadde-17/Portfolio-website/blob/main/src/assets/login.png?raw=true",
+      title: "Login",
+      subtitle: "Backend, Password Validation",
+      handle: "Full Stack",
+      borderColor: "#0EA5E9",
+      gradient: "linear-gradient(180deg, #0EA5E9, #000)",
+      url: " ",
+    },
+    {
+      image:
+        "https://github.com/Prasanthgadde-17/Portfolio-website/blob/main/src/assets/todo2.png?raw=true",
+      title: "To-Do Application",
+      subtitle: "HTML5, CSS3, JS",
+      handle: "Frontend Project",
+      borderColor: "#D97706",
+      gradient: "linear-gradient(190deg, #D97706, #000)",
+      url: "https://github.com/Prasanthgadde-17/TO-DO-",
+    },
+  ];
+
+  return (
+    <>
+      <div
+        className="border border-white/40 m-10 rounded-xl shadow h-[1800px] sm:h-[1200px] md:h-[1050px]"
+        id="project"
+      >
+        <div className="text-white font-bold mt-10 text-center">
+          <div className="font-light text-lg">Projects</div>
+          <div className="text-4xl mt-4">What I’ve Built</div>
+          <p className="mt-5 w-full md:w-1/3 text-gray-400 mx-auto text-md px-4 ">
+            From learning the basics to building full-stack applications, here
+            are some of the key projects that helped me grow as a developer.
+          </p>
+        </div>
+
+        <div className="container mt-5 p-5 ">
+          <div className="relative h-[1500px] sm:h-[1000px] md:h-[700px] overflow-auto ml-3">
+            <ChromaGrid
+              items={items}
+              radius={300}
+              damping={0.45}
+              fadeOut={0.4}
+              ease="power3.out"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Project;
